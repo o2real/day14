acef065571f998d839618c5fa70ff0d3f93aefbf
Kingcode <136673119+o2real@users.noreply.github.com>
2024-06-11 06:14:59 +0000

    higher lower game .fin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from art import logo,vs
 from game_data import data
 import random
+from replit import clear
 
 def format_data(account):
   """데이터를 정렬해서 출력"""
@@ -18,44 +19,50 @@ def check_answer(guess, a_followers, b_followers):
 
 #display art
 print(logo)
-
 score = 0 
+game_should_continue = True
 
-# Generate a random account from the game data. 
-account_a = random.choice(data)
 account_b = random.choice(data)
-if account_a == account_b :
-  account_b = random.choice(data)
-
-
-print(f"Compare A: {format_data(account_a)}.")
-
-print(vs)
-
-print(f"Against B: {format_data(account_b)}.")
-
-
-# 사용자한테 추측한거 물어보기
-guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-
-# 사용자가 답을 맞췄는지 확인 
-##각각의 팔로워 수를 확인
-a_follower_count = account_a["follower_count"]
-b_follower_count = account_b["follower_count"]
-
-is_correct = check_answer(guess, a_follower_count, b_follower_count)
-
-# 사용자에게 사용자의 추측에 대한 피드백 주기
-if is_correct :
-  score += 1
-  print(f"You're right! Current score: {score}.")
-else:
-  print(f"Sorry, that' wrong Final score: {score}.")
-
-# 점수 저장
 
 # 게임을 반복하게 만들기
+while game_should_continue :
 
-# 포지션 B를 A로 올리기
+  # Generate a random account from the game data. 
 
-# 매 라운드마다 초기화하기 클리어 스크린  
+  # 포지션 B를 A로 올리기
+  account_a = account_b
+  account_b = random.choice(data)
+
+  while account_a == account_b :
+    account_b = random.choice(data)
+
+  print(f"Compare A: {format_data(account_a)}.")
+
+  print(vs)
+
+  print(f"Against B: {format_data(account_b)}.")
+
+
+  # 사용자한테 추측한거 물어보기
+  guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+  # 사용자가 답을 맞췄는지 확인 
+  ##각각의 팔로워 수를 확인
+  a_follower_count = account_a["follower_count"]
+  b_follower_count = account_b["follower_count"]
+
+  is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+  # 매 라운드마다 초기화하기 클리어 스크린  
+  clear()
+  print(logo)
+  # 사용자에게 사용자의 추측에 대한 피드백 주기
+  # 점수 저장
+  if is_correct :
+    score += 1
+    print(f"You're right! Current score: {score}.")
+  else:
+    print(f"Sorry, that' wrong Final score: {score}.")
+    game_should_continue = False
+
+
